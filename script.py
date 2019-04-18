f98b154dc94d72280fe0b3fd5044666ee9145d98
mani <sadhak001@gmail.com>
2019-04-18 19:58:03 +0100

    Apply the changes to the app to make the three APIs work

--- script.py
+++ script.py
@@ -1,9 +1,50 @@
-from flask import Flask
-from serve import useless_function  
+import json
+from flask import Flask, request
+from serve import get_keywords_api
 
-app=Flask(__name__)
+# I've commented out the last import because it won't work in kernels, 
+# but you should uncomment it when we build our app tomorrow
+
+# create an instance of Flask
+app = Flask(__name__)
 
 # Define our "ping" end point
 @app.route('/ping')
-def useless_output():
-  return(useless_function())
+def ping():
+  return("Pong!")
+
+# Define a post method for our API: python packages.
+@app.route('/extractpackages/python', methods=['POST'])
+def extract_python_packages():
+    extractpackages("python")
+
+# Define a post method for our API: R packages.
+@app.route('/extractpackages/r', methods=['POST'])
+def extract_r_packages():
+    extractpackages("r")
+
+def extractpackages(language="python"):
+    """ 
+    Takes in a json file, extracts the keywords &
+    their indices and then returns them as a json file.
+    """
+    # the data the user input, in json format
+    input_data = request.json
+
+    # load our pre-trained model & function
+    keywords_api = get_keywords_api(language)
+
+    # use our API function to get the keywords
+    output_data = keywords_api(input_data)
+
+    # convert our dictionary into a .json file
+    # (returning a dictionary wouldn't be very
+    # helpful for someone querying our API from
+    # java; JSON is more flexible/portable)
+
+    print(output_data)
+    response = json.dumps(output_data)
+
+    print(response)
+    # return our json file
+    return response
